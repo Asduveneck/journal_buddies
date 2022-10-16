1890aea5bc7f125bc98a99fc7d3fef7040491158
Asduveneck <asduveneck@ucdavis.edu>
2022-10-16 00:50:37 -0700

    Rename columns

--- db/migrate/20221013094404_create_prompts.rb
+++ db/migrate/20221013094404_create_prompts.rb
@@ -1,7 +1,7 @@
 class CreatePrompts < ActiveRecord::Migration[7.0]
   def change
     create_table :prompts do |t|
-      t.string :prompt
+      t.string :title
       t.boolean :editable
       t.date :scheduled_date
       t.references :journal, null: false, foreign_key: true
--- db/migrate/20221014080341_create_entries.rb
+++ db/migrate/20221014080341_create_entries.rb
@@ -1,7 +1,7 @@
 class CreateEntries < ActiveRecord::Migration[7.0]
   def change
     create_table :entries do |t|
-      t.text :entry
+      t.text :text_content
       t.boolean :editable
       t.references :prompt, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
