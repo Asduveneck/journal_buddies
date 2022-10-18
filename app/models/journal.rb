9f0be368f5567e30f49a396410655e7e54b36a08
Alex Duveneck <alexander.duveneck@gmail.com>
2022-10-17 22:03:55 -0700

    Actually fix has many through association

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -1,5 +1,6 @@
 class Journal < ApplicationRecord
   has_many :prompts
-  has_many :users, :through => :journals_users
+  has_many :journals_users
+  has_many :users, through: :journals_users
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+    has_many :journals_users
     has_many :journals, through: :journals_users
 end
