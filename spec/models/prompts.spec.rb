398cb3f24a253ce551422a1c5dd882d42b98e8b7
Asduveneck <asduveneck@ucdavis.edu>
2022-11-20 21:15:44 -0800

    draft prompts specs

---
+++ spec/models/prompts.spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe ::Entry, type: :model do
+  describe 'initialization' do
+    it 'is invalid without a journal' do
+    end
+
+    it 'is valid without a parent prompt' do
+    end
+
+    it 'can have entries' do
+    end
+  end
+
+  describe '#sibling_prompts' do
+    # subject prompt.sibling_prompts
+    describe 'when there is no parent prompt' do
+
+    end
+
+    describe 'when there is a parent prompt' do
+    end
+  end
+end
