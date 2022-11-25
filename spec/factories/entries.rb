da5ae62974c90c893088e4b095f83292456b3997
Alex Duveneck <alexander.duveneck@gmail.com>
2022-11-25 10:46:02 -0800

    draft efine simple mocks/factories

---
+++ spec/factories/entries.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :entry do
+    text: { 'Today was wonderful. I had lots of yummy food.' }
+  end
+end
---
+++ spec/factories/prompts.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :prompt do
+    title: { 'How was today?' }
+  end
+end
