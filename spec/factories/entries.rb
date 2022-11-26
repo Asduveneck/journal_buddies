15d5297173f5a69d3b0ef427ef5f50358432efc1
Alex Duveneck <alexander.duveneck@gmail.com>
2022-11-25 23:14:56 -0800

    Fix some typos

--- spec/factories/entries.rb
+++ spec/factories/entries.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :entry do
-    text { 'Today was wonderful. I had lots of yummy food.' }
+    text_content { 'Today was wonderful. I had lots of yummy food.' }
   end
 end
--- spec/models/entries.spec.rb
+++ spec/models/entries.spec.rb
@@ -5,24 +5,26 @@ RSpec.describe ::Entry, type: :model do
     let(:params) { nil }
 
     let(:entry_text) { 'journal entry response' }
+    let(:user) { build_stubbed(:user) }
+    let(:prompt) { build_stubbed(:prompt) }
 
     it 'is invalid without a user' do
-      entry = Entry.new(text: entry_text, user_id: nil, prompt_id: 1)
+      entry = Entry.new(text_content: entry_text, user_id: nil, prompt_id: prompt.id)
       expect(entry).to_not be_valid
     end
 
     it 'is invalid without a prompt' do
-      entry = Entry.new(text: entry_text, user_id: 1, prompt_id: nil)
+      entry = Entry.new(text_content: entry_text, user_id: user.id, prompt_id: nil)
       expect(entry).to_not be_valid
     end
 
     it 'is invalid without a text' do
-      entry = Entry.new(text: nil, user_id: 1, prompt_id: 1)
+      entry = Entry.new(text_content: nil, user_id: user.id, prompt_id: prompt.id)
       expect(entry).to_not be_valid
     end
 
   it 'is valid with a user, prompt and text' do
-    entry = Entry.new(text: entry_text, user_id: 1, prompt_id: 1)
+    entry = Entry.new(text_content: entry_text, user_id: user.id, prompt_id: prompt.id)
     expect(entry).to be_valid
   end
 
@@ -30,23 +32,23 @@ RSpec.describe ::Entry, type: :model do
 
   describe '#prompt_text' do
     # Warning: Factorybot / Factorygirl not installed yet...
-    let(:prompt) { build_stubbed(:prompt, prompt: "What did you eat today?") }
+    let(:prompt) { build_stubbed(:prompt, title: "What did you eat today?") }
     let(:user) { build_stubbed(:user, first_name: "Sam") }
     let(:entry) { build_stubbed(:entry, prompt_id: prompt.id, user_id: user.id) }
 
     it 'can access the prompt text' do
-      expect(entry.prompt).to eq prompt.prompt
+      expect(entry.prompt).to eq prompt.title
     end
   end
 
   describe '#sibling_entries' do
-    let(:prompt) { stubbed(:prompt, prompt: 'What did you eat today?') }
+    let(:prompt) { build_stubbed(:prompt, title: 'What did you eat today?') }
     let(:user_one) { build_stubbed(:user, first_name: 'Sam') }
     let(:user_two) { build_stubbed(:user, first_name: 'Fran') }
     let(:user_three) { build_stubbed(:user, first_name: 'Bill') }
-    let(:entry_one) { build_stubbed(:entry, text: 'entry one', prompt_id: prompt.id, user_id: user_one.id) }
-    let(:entry_two) { build_stubbed(:entry, text: 'entry two', prompt_id: prompt.id, user_id: user_two.id) }
-    let(:entry_three) { build_stubbed(:entry, text: 'entry three', prompt_id: prompt.id, user_id: user_three.id) }
+    let(:entry_one) { build_stubbed(:entry, text_content: 'entry one', prompt_id: prompt.id, user_id: user_one.id) }
+    let(:entry_two) { build_stubbed(:entry, text_content: 'entry two', prompt_id: prompt.id, user_id: user_two.id) }
+    let(:entry_three) { build_stubbed(:entry, text_content: 'entry three', prompt_id: prompt.id, user_id: user_three.id) }
 
     it 'returns all sibling entries for the same parent prompt' do
       expected_entries = [entry_one, entry_two, entry_three]
