80916ea60651e270b2a487691b9f815d80644a3f
Alex Duveneck <alexander.duveneck@gmail.com>
2022-11-26 22:45:51 -0800

    Define shortcuts to common functions that now need to be prefaced w/ docker compose

---
+++ bin/run_docker_shortcuts.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Please type in a valid command:"
+  read COMMAND
+else
+  COMMAND=$1
+fi
+
+shift
+
+case $COMMAND in
+  rails-g-migration | r-g-m )
+    docker compose run web bin/rails generate migration "$@"
+  ;;
+
+  rails-run-migration | rails-db-migrate | r-db-m )
+    docker compose run web bin/rails db:migrate "$@"
+  ;;
+
+  rspec )
+    docker compose run web bundle exec rspec "$@"
+  ;;
+
+  * )
+    echo "Invalid command"
+  ;;
+esac
