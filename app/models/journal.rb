0a697807a24c2721a939ad43d1d35e65242bc4d8
Alex Duveneck <alexander.duveneck@gmail.com>
2023-02-10 14:04:22 -0800

    Draft recurring prompts filter method

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -6,6 +6,10 @@ class Journal < ApplicationRecord
 
   # Recurring prompts and prompts should be handled
   def pending_recurring_prompts
-    recurring_prompts.filter { |recurring_prompt| recurring_prompt.create_prompt? }
+    @pending_recurring_prompts ||= recurring_prompts.filter { |recurring_prompt| recurring_prompt.create_prompt? }
+  end
+
+  def create_pending_recurring_prompts
+    pending_recurring_prompts
   end
 end
