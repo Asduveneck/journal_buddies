ef8535ba18e46c99a5287737b8371eca644d402e
Alex Duveneck <alexander.duveneck@gmail.com>
2023-02-13 00:07:20 -0800

    Install devise/rubocop and run basic setup
    install rubocop and devise
    
    Setup config / g devise User
    
    More devise install boilerplate
    
    Modify then run devise user db migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
-    has_many :journals_users
-    has_many :journals, through: :journals_users
+  has_many :journals_users
+  has_many :journals, through: :journals_users
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
