8f57e8203e6024e6fa1457cda2b13bfaa167049b
Alex Duveneck <alexander.duveneck@gmail.com>
2023-05-20 18:59:29 -0700

    Start permission policies

---
+++ app/services/journals_users_policies/journals_policy.rb
@@ -0,0 +1,52 @@
+# Draft class to restrict who can view, edit, or delete journals
+# In short, a journal can be viewed by any participant, or anyone if the journal is public
+# a journal can only be deleted/edited by an admin for that journal
+
+# TODOS:
+# 1. simplify the user_role to be a string / enum
+# 2. make the visibility to be a boolean. Visible or private.
+# 3. helper for user_role == 'admin' haha.
+
+class JournalsPolicy
+  def initialize(journal, user, journals_users)
+    @journal = journal
+    @user = user
+    @journals_users = journals_users
+  end
+
+  def show?
+    return true if @journal.public?
+    
+    return user_in_journals_users?
+  end
+
+  def delete?
+    return false unless user_in_journals_users?
+
+    return user_role == 'admin'
+  end
+
+  def edit?
+    return false unless user_in_journals_users?
+
+    return user_role == 'admin'
+  end
+
+  def add_user?
+    return user_role == 'admin'
+  end
+
+  private
+
+  def journal_user
+    @journal_user ||= @journals_users.include(have_attributes(user_id: @user.id))
+  end
+
+  def user_in_journals_users?
+    @user_in_journals_users? ||= @journal_user.present?
+  end
+
+  def user_role
+    @user_role ||= @journal_user&.role || ''
+  end
+end
