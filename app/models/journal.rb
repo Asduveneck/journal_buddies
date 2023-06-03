f86134de052353ad8e4e73bceb9318aef2e4e536
Alex Duveneck <alexander.duveneck@gmail.com>
2023-06-03 16:11:09 -0700

    fix nested attributes

--- app/models/journal.rb
+++ app/models/journal.rb
@@ -2,7 +2,7 @@ class Journal < ApplicationRecord
   has_many :prompts
   has_many :recurring_prompts
   has_many :journals_users
-  accepts_nested_attributes_for :journals_users, proc { |attributes| attributes['user_id'].blank? }
+  accepts_nested_attributes_for :journals_users, reject_if: proc { |attributes| attributes['user_id'].blank? }
   has_many :users, through: :journals_users
 
   validates :name, presence: true
