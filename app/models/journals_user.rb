4e84a77c0ca0e2410e6ae7728d4ac0321ddcc99e
Alex Duveneck <alexander.duveneck@gmail.com>
2023-06-05 19:20:18 -0700

    reorder to fix enum

--- app/models/journals_user.rb
+++ app/models/journals_user.rb
@@ -2,6 +2,6 @@ class JournalsUser < ApplicationRecord
   belongs_to :journal
   belongs_to :user
 
-  enum :user_role, USER_ROLES,  _prefix: :user_role
   USER_ROLES = { admin: 'admin', prompt_writer: 'prompt_writer', participant: 'participant', viewer: 'viewer' }.freeze
+  enum :user_role, USER_ROLES,  _prefix: :user_role
 end
