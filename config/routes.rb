af1e8e3c04ef729b139cfdc73caf06fdcf427065
Alex Duveneck <alexander.duveneck@gmail.com>
2023-06-15 03:49:48 -0700

    fix port for cors; fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  # Draft!
   namespace :api, defaults: {format: :json} do
-      devise_for :users, path: '', path_names: {
-        sign_in: 'login',
-        sign_out: 'logout',
-        registration: 'signup'
-      },
-      controllers: {
-        sessions: 'users/sessions',
-        registrations: 'users/registrations'
-      }
-
     # make sure to not have a general journals index
     resources :journals do
       resources :prompts
