29de966d23423108f0781c77f54d9f735782b8ba
Alex Duveneck <alexander.duveneck@gmail.com>
2023-08-31 02:59:50 -0700

    Add new factory for journals association on user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,16 @@ FactoryBot.define do
     first_name { 'Sam' }
     last_name { 'Duran' }
     email { 'hello@world.com' }
+
+    factory :user_with_journals do
+      transient do
+        journals_count { 2 }
+      end
+
+      after(:create) do |user, evaluator|
+        journals = build_stubbed_list(:journal, evaluator.journals_count)
+        user.journals = journals
+      end
+    end
   end
 end
